78264b07d77de5372578640b2f08526370dc5ab5
Kyungil Park <kyungil.park@navercorp.com>
2022-05-23 18:05:33 +0900

    클론 코딩으로 시작하는 Next.js - 3,4장 실습

--- clone-coding/next-todo/pages/index.tsx
+++ clone-coding/next-todo/pages/index.tsx
@@ -1,7 +1,23 @@
 import { NextPage } from "next";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 20px;
+`;
 
 const index: NextPage = () => {
-  return <div>hello Typescript</div>;
+  return (
+    <Container>
+      <h2>Hello Styled-components</h2>
+      <h2>Hello Styled-components</h2>
+      <h2>Hello Styled-components</h2>
+      <ul>
+        <li>Hello Styled-components</li>
+      </ul>
+      <p>Hello Styled-components</p>
+      <span>Hello Styled-components</span>
+    </Container>
+  );
 };
 
 export default index;
