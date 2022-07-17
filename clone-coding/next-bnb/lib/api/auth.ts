e3669785b94941138738b3881b898fd94e83fda8
Kyungil Park <kyungil.park@navercorp.com>
2022-07-17 17:16:25 +0900

    클론 코딩으로 시작하는 Next.js - 10.4 실습

---
+++ clone-coding/next-bnb/lib/api/auth.ts
@@ -0,0 +1,13 @@
+import axios from "./index";
+
+// 회원가입 body
+interface SignUpAPIBody {
+    email: string;
+    firstname: string;
+    lastname: string;
+    password: string;
+    birthday: string;
+}
+
+// 회원가입 api
+export const signupAPI = (body: SignUpAPIBody) => axios.post("/api/auth/signup", body);
